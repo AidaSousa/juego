20fefa5b9960302729aa392ec297a5566f13139c
AidaSousa <117081796+AidaSousa@users.noreply.github.com>
2023-01-31 08:38:19 +0100

    Quiz preguntas comunidades de desarrollo

---
+++ src/preguntas.js
@@ -0,0 +1,94 @@
+const preguntas = [
+    {
+        titulo: "¿Qué es una comunidad de desarrolladores?",
+        opciones: [
+            { textoRespuesta: "Un equipo de desarrolladores que trabajan juntos en un proyecto específico", isCorrect: false },
+            { textoRespuesta: "Una red de programadores que colaboran en la creación de código abierto", isCorrect: false },
+            { textoRespuesta: "Un grupo de personas que se reúnen para discutir y compartir conocimientos sobre desarrollo de software", isCorrect: true },
+            { textoRespuesta: "Un edificio en el que sólo viven desarrolladores", isCorrect: false},
+        ],
+    },
+    {
+        titulo: "¿Qué beneficios proporciona unirse a una comunidad de desarrolladores?",
+        opciones: [
+            { textoRespuesta: "Aumenta la productividad y la eficiencia en el trabajo", isCorrect: false },
+            { textoRespuesta: "Permite establecer contactos profesionales y posibles oportunidades laborales", isCorrect: false },
+            { textoRespuesta: "Todas son correctas", isCorrect: true },
+            { textoRespuesta: "Permite aprender nuevas habilidades y tecnologías, colaborar en proyectos y contar con mentores", isCorrect: false },
+        ],
+    },
+    {
+        titulo: "¿Qué es una comunidad de código abierto?",
+        opciones: [
+            { textoRespuesta: "Un grupo de personas que colaboran en el desarrollo de software de código cerrado", isCorrect: false },
+            { textoRespuesta: "Un grupo de personas que colaboran en el desarrollo de software de código abierto", isCorrect: true },
+            { textoRespuesta: "Un grupo de personas que colaboran en el desarrollo de proyectos de diseño", isCorrect: false },
+            { textoRespuesta: "Un grupo de personas que colaboran en el desarrollo de proyectos de marketing", isCorrect: false },
+        ],
+    },
+    {
+        titulo: "¿Qué tipo de actividades suelen realizar las comunidades de desarrolladores?",
+        opciones: [
+            { textoRespuesta: "Conferencias, talleres y charlas técnicas", isCorrect: false },
+            { textoRespuesta: "Coding sprints y hackathons", isCorrect: false },
+            { textoRespuesta: "Intercambio de experiencias y prácticas entre los miembros de la comunidad", isCorrect: false },
+            { textoRespuesta: "Todas son correctas", isCorrect: true },
+        ],
+    },
+    {
+        titulo: "¿Qué es una comunidad Open Source?",
+        opciones: [
+            { textoRespuesta: "Una comunidad de personas que se dedican a la creación de hardware de código abierto", isCorrect: false },
+            { textoRespuesta: "Todas son correctas", isCorrect: true },
+            { textoRespuesta: "Un grupo de desarrolladores que trabajan juntos para crear software de código abierto", isCorrect: false },
+            { textoRespuesta: "Una comunidad de usuarios que contribuyen con reportes de bugs y sugerencias para mejorar software de código abierto", isCorrect: false },
+        ],
+    },
+    {
+       titulo:"¿Qué es el Open Source y cuál es su importancia para las comunidades de desarrolladores?",
+       opciones: [
+        { textoRespuesta: "El Open Source es una técnica de seguridad en el desarrollo de software donde se utilizan códigos cifrados para proteger el software de posibles ataques", isCorrect: false },
+        { textoRespuesta: "El Open Source es una técnica de software gratuita que permite a los usuarios utilizar el software de manera libre y sin restricciones", isCorrect: false },
+        { textoRespuesta: "El Open Source es una filosofía de desarrollo de software donde el código fuente es de libre acceso y modificación, es importante porque permite a los desarrolladores colaborar y mejorar el software de manera colectiva", isCorrect: true },
+        { textoRespuesta: "Ninguna es correcta", isCorrect: false },
+       ],
+    },
+    {
+        titulo: "¿Qué ventajas ofrece el trabajar en comunidades de desarrolladores Open Source?",
+        opciones: [
+            { textoRespuesta: "Acceso a un gran número de recursos y herramientas de desarrollo", isCorrect: false },
+            { textoRespuesta: "Posibilidad de colaborar con desarrolladores de todo el mundo", isCorrect: false },
+            { textoRespuesta: "Oportunidades para aprender y mejorar habilidades en una letiedad de proyectos", isCorrect: false },
+            { textoRespuesta: "Todas son correctas", isCorrect: true },
+        ],
+    },
+    {
+        titulo: "¿Qué es un meetup de desarrolladores?",
+        opciones: [
+            { textoRespuesta: "Un evento donde los desarrolladores se reúnen para discutir temas técnicos y compartir experiencias", isCorrect: true },
+            { textoRespuesta: "Una plataforma en línea donde los desarrolladores pueden colaborar en proyectos", isCorrect: false },
+            { textoRespuesta: "Un servicio de mentoría para desarrolladores principiantes", isCorrect: false },
+            { textoRespuesta: "Todas son correctas", isCorrect: false },
+        ],
+    },
+    {
+        titulo: "¿Qué ventajas tiene para un desarrollador asistir a una conferencia de desarrolladores?",
+        opciones: [
+            { textoRespuesta: "Pueden aprender sobre las últimas tendencias y herramientas en el campo", isCorrect: false },
+            { textoRespuesta: "Pueden conectarse con otros desarrolladores y expandir su red de contactos profesionales", isCorrect: false },
+            { textoRespuesta: "Pueden obtener conocimientos y perspectivas valiosas de expertos en el campo", isCorrect: false },
+            { textoRespuesta: "Todas son correctas", isCorrect: true },
+        ],
+    },
+    {
+        titulo: "¿Qué es un Hackathon?",
+        opciones: [
+            { textoRespuesta: "Una conferencia para discutir temas relacionados con el hacking y la seguridad informática", isCorrect: false },
+            { textoRespuesta: "Un evento de corta duración donde los participantes trabajan en equipos para desarrollar soluciones o prototipos para un desafío o problema específico, generalmente relacionado con tecnología y software", isCorrect: true },
+            { textoRespuesta: "Una competencia de programación para desarrolladores principiantes", isCorrect: false },
+            { textoRespuesta: "Un curso sobre ciberseguridad", isCorrect: false },
+        ],
+    },
+];
+
+export default preguntas;
